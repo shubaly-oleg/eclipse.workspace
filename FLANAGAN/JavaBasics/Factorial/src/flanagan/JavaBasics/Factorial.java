f9663b5ae4a1099358d70c66bce1aec14be8d016
Shubalyi Oleg <shubaly.oleg@gmail.com>
2016-05-27 10:34:10 +0300

    factorial updated

--- FLANAGAN/JavaBasics/Factorial/src/flanagan/JavaBasics/Factorial.java
+++ FLANAGAN/JavaBasics/Factorial/src/flanagan/JavaBasics/Factorial.java
@@ -3,8 +3,12 @@ package flanagan.JavaBasics;
 public class Factorial {
 	public static int factorial(int x) throws IllegalAccessException {
 		if (x < 0) throw new IllegalAccessException("x must be > 0");
-		int fact =0;
-		for (int i = 2; i <= x; i++) fact = fact * i;
+		int fact = 1;
+		for (int i = 2; i <= x; i++) {
+//			System.out.println(i);
+//			System.out.println(fact);
+			fact = fact * i;
+		}
 		return fact;
 	}
 }
